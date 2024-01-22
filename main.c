0f0847b0af93443849b6214fbbe6b202426a4bc1
Luca Ignatescu <lucaignatescu003@gmail.com>
2024-01-22 12:53:06 +0200

    Added gitignore

--- main.c
+++ main.c
@@ -2,19 +2,23 @@
 #define _DEFAULT_SOURCE
 
 #include "keymaps.h"
-#include <signal.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 typedef unsigned short int usint;
 typedef struct termios termios;
+
+typedef struct Line{
+  char * text;
+} Line;
 
 typedef struct Config {
   termios initialConfig;
@@ -22,6 +26,23 @@ typedef struct Config {
 } Config;
 
 Config config;
+
+
+void renderLine(const Line * line){
+  if (config.nCols == 0) return;
+  const size_t chunk = config.nCols;
+  char * buf = (char *) malloc((chunk + 1) * sizeof(char));
+  // memset(buf, 0, 0);
+
+  buf[chunk] = '\0';
+
+  size_t length = strlen(line->text);
+  
+  for(size_t i = 0; i < length; i += chunk){
+    memcpy(buf, line + i, chunk);
+    write()
+  }
+}
 
 void terminalMakeRaw() {
   system("tput smcup");
@@ -57,30 +78,32 @@ void resetScreen() {
   write(STDOUT_FILENO, buf, strlen(buf));
 }
 
-void getTerminalSize(){
+void getTerminalSize() {
   struct winsize ws;
-  if(ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1)
+  if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1)
     exit(1);
   config.nRows = ws.ws_row;
   config.nCols = ws.ws_col;
 }
 
-void handleWINCH(int){
+void handleWINCH(int) {
   getTerminalSize();
   const char buf[100];
-  sprintf(buf, "%d %d" NEXTLINE, config.nRows, config.nCols);
+  sprintf((char *)buf, "%d %d" NEXTLINE, config.nRows, config.nCols);
   write(STDOUT_FILENO, buf, strlen(buf));
 }
 
-void registerWINCHandler(){
-  struct sigaction act;
-  memset(&act, 0, sizeof(act));
-  act.sa_handler = handleWINCH;
-  sigaction(SIGWINCH, &act, NULL);
-
+void registerWINCHandler() {
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(sa));
+  sa.sa_flags = SA_RESTART;
+  sa.sa_handler = handleWINCH;
+  sigaction(SIGWINCH, &sa, NULL);
 }
 
+void parseFile(){
 
+}
 
 void init() {
   memset(&config, 0, sizeof(config));
